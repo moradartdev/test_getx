48019132f88c4680fa62061378e54a8441ebbba2
¨mourad¨ <mouradhalhou83@gmail.com>
2026-01-05 09:51:33 +0100

    creet Pagethwo controller and 4  methods to inecrement and decrement value for test update by obs

---
+++ lib/controllers/page_thwo_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+
+class PageThwoController extends GetxController {
+  RxInt value1 = 0.obs;
+  RxInt value2 = 0.obs;
+
+  void inecrementValue1() {
+    value1++;
+  }
+
+  void decrementValue1() {
+    value1--;
+  }
+
+  void inecrementValue2() {
+    value2++;
+  }
+
+  void decrementValue2() {
+    value2--;
+  }
+
+  int get sum => value1.value + value2.value;
+}
