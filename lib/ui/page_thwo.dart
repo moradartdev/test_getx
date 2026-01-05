915ff27efe365f7f4aaa97a0523ecc2f24159556
¨mourad¨ <mouradhalhou83@gmail.com>
2026-01-05 09:49:50 +0100

    text getx builder

--- lib/ui/page_thwo.dart
+++ lib/ui/page_thwo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+import 'package:test_getx/controllers/page_thwo_controller.dart';
 
 class PageThwo extends StatelessWidget {
   const PageThwo({super.key});
@@ -6,15 +8,76 @@ class PageThwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Welcome to page two screen',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 19,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
+      appBar: AppBar(title: Text('PAGE THWO')),
+      body: GetX(
+        init: PageThwoController(),
+        builder: (controller) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('${controller.value1.value}'),
+                SizedBox(height: 20),
+                Text('${controller.value2.value}'),
+                SizedBox(height: 20),
+                Text('${controller.sum}'),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: MaterialButton(
+                    color: const Color.fromARGB(255, 2, 53, 94),
+                    onPressed: () {
+                      controller.inecrementValue1();
+                    },
+                    child: Text(
+                      'add Value one',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: MaterialButton(
+                    onPressed: () {
+                      controller.decrementValue1();
+                    },
+                    color: const Color.fromARGB(255, 2, 53, 94),
+                    child: Text(
+                      'remove value one',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: MaterialButton(
+                    color: const Color.fromARGB(255, 2, 53, 94),
+                    onPressed: () {
+                      controller.inecrementValue2();
+                    },
+                    child: Text(
+                      'add value 2',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: MaterialButton(
+                    onPressed: () {
+                      controller.decrementValue2();
+                    },
+                    color: const Color.fromARGB(255, 2, 53, 94),
+                    child: Text(
+                      'remove value 2',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
